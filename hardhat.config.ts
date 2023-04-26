18eb55cb347a84b2e8d121041bc50337d82ed73b
Agustinus Theodorus <agustinustheoo@gmail.com>
2023-04-26 22:00:11 +0700

    feat: added gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,11 @@ import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY ?? "",
+  },
 };
 
 export default config;
